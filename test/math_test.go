71b813bd986a41e410ed59883a960a4e864d87e8
augustczar <augustczarramos@gmail.com>
2023-12-19 12:29:41 -0300

    🌟 feat: Added sum function

---
+++ test/math_test.go
@@ -0,0 +1,11 @@
+package test
+
+import "testing"
+
+func TestSoma(t *testing.T) {
+	total := Soma(15, 15)
+
+	if total != 30 {
+		t.Errorf("Sum result is invalid: Result: %d. Expected: %d", total, 30)
+	}
+}
